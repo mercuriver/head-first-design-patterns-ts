55a2bd52195499d730d1aa88c991b32e747f3312
mercuriver <mercuriver@gmail.com>
2022-10-02 21:48:38 +0900

    test(strategy): 물오리 테스트 케이스 작성

--- patterns/StrategyPattern/index.spec.ts
+++ patterns/StrategyPattern/index.spec.ts
@@ -1,11 +1,21 @@
 import { expect } from "chai";
 
-import Duck, { meta } from "./Duck";
+import { meta } from "./Duck";
+import MallardDuck, { meta as MallardDuckMeta } from "./MallardDuck";
 
-describe("Duck 테스트", function () {
-  const duck = new Duck();
+describe("물오리 테스트", function () {
+  const duck = new MallardDuck();
 
-  it("오리 헤엄", function () {
+  it("자기 소개", function () {
+    expect(duck.display()).to.equal(MallardDuckMeta.message_display);
+  });
+  it("헤엄", function () {
     expect(duck.swim()).to.equal(meta.message_swim);
   });
+  // it("꽥꽥", function () {
+  //   expect(duck.quack()).to.equal("꽥");
+  // });
+  // it("비행", function () {
+  //   expect(duck.fly()).to.equal("날고 있어요!!");
+  // });
 });
