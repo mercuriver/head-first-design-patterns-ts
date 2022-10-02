cbb562eb2f2d358ea19c6445f93aff03a00ba9ab
mercuriver <mercuriver@gmail.com>
2022-10-02 23:07:40 +0900

    test(strategy): 전체 테스트 케이스 작성

--- patterns/strategy/index.spec.ts
+++ patterns/strategy/index.spec.ts
@@ -6,21 +6,53 @@ import defaultMessage, {
   extraMessages,
 } from "./index.meta";
 
-describe("물오리 테스트", function () {
 import MallardDuck from "./MallardDuck";
+
+class ModelDuck {}
+class QuackMuted {}
+class FlyRocketPowered {}
+
+let seq = 1;
+describe(`[${seq++}] 물오리 테스트`, function () {
   const duck = new MallardDuck();
 
   it("자기 소개", function () {
     expect(duck.display()).to.equal(mallardDuck.message_display);
   });
   it("헤엄", function () {
-    expect(duck.swim()).to.equal(meta.message_swim);
-  });
-  // it("꽥꽥", function () {
-  //   expect(duck.quack()).to.equal("꽥");
-  // });
-  // it("비행", function () {
-  //   expect(duck.fly()).to.equal("날고 있어요!!");
-  // });
     expect(duck.swim()).to.equal(defaultMessage.message_swim);
+  });
+  it("꽥꽥", function () {
+    expect(duck.performQuack()).to.equal(mallardDuck.message_quack);
+  });
+  it("비행", function () {
+    expect(duck.performFly()).to.equal(mallardDuck.message_fly);
+  });
+});
+
+describe(`[${seq++}] 모형 오리 테스트`, function () {
+  const duck = new ModelDuck();
+
+  it("자기 소개", function () {
+    expect(duck.display()).to.equal(modelDuck.message_display);
+  });
+  it("헤엄", function () {
+    expect(duck.swim()).to.equal(defaultMessage.message_swim);
+  });
+  it("꽥꽥", function () {
+    expect(duck.performQuack()).to.equal(modelDuck.message_quack);
+  });
+  it("비행", function () {
+    expect(duck.performFly()).to.equal(modelDuck.message_fly);
+  });
+
+  it("꽥꽥 방식 변경", function () {
+    duck.setQuackBehavior(new QuackMuted());
+    expect(duck.performQuack()).to.equal(extraMessages.message_quack_mute);
+  });
+
+  it("비행 방식 변경", function () {
+    duck.setFlyBehavior(new FlyRocketPowered());
+    expect(duck.performFly()).to.equal(extraMessages.message_fly_rocket);
+  });
 });
