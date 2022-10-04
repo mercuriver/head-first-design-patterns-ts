1e0c1cd365f0985706f59b80a1dacbe8a3e834bd
mercuriver <mercuriver@gmail.com>
2022-10-04 22:11:33 +0900

    feat(observer): WeatherData class 작성

---
+++ patterns/observer/WeatherData.ts
@@ -0,0 +1,46 @@
+import Subject from "./interfaces/Subject";
+import Observer from "./interfaces/Observer";
+
+class WeatherData implements Subject {
+  #observers: Observer[];
+  #temperature: number;
+  #humidity: number;
+  #pressure: number;
+
+  constructor(temp: number, humidity: number, pressure: number) {
+    this.#observers = [];
+    this.#temperature = temp;
+    this.#humidity = humidity;
+    this.#pressure = pressure;
+  }
+
+  registerObserver(observer) {
+    this.#observers.push(observer);
+  }
+
+  removeObserver(observer) {
+    // Todo: remove #observers
+  }
+
+  notiObservers() {
+    // Todo: #observers.each.update
+  }
+
+  get observersCount() {
+    return this.#observers.length;
+  }
+
+  get temperature() {
+    return this.#temperature;
+  }
+
+  get humidity() {
+    return this.#humidity;
+  }
+
+  get pressure() {
+    return this.#pressure;
+  }
+}
+
+export default WeatherData;
