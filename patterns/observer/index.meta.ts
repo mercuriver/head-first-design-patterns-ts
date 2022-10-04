72d75f1e1de05fec6b9e8e4f85172bb170d7c25e
mercuriver <mercuriver@gmail.com>
2022-10-04 22:10:26 +0900

    feat(observer): 테스트에 사용 될 mock 데이터 추가

---
+++ patterns/observer/index.meta.ts
@@ -0,0 +1,11 @@
+const weatherMockData: {
+  temperature: number[];
+  humidity: number[];
+  pressure: number[];
+} = {
+  temperature: [26],
+  humidity: [64],
+  pressure: [1013.25], // 단위: hPa
+};
+
+export default weatherMockData;
