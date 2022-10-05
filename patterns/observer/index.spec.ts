4161bcd32fe748ab407add1f6bcb10bff7d71fe6
mercuriver <mercuriver@gmail.com>
2022-10-05 21:50:15 +0900

    test(observer): 전체 테스트 케이스 작성

--- patterns/observer/index.spec.ts
+++ patterns/observer/index.spec.ts
@@ -2,22 +2,122 @@ import { expect } from "chai";
 
 import WeatherData from "./WeatherData";
 
-import weatherMockData from "./index.meta";
+import CurrentConditionDisplay from "./CurrentConditionDisplay";
+import StatisticDisplay from "./StatisticDisplay";
+import ForecastDisplay from "./ForecastDisplay";
 
-let seq = 1;
-describe(`[${seq++}] 초기`, function () {
-  const {
-    temperature: [t],
-    humidity: [h],
-    pressure: [p],
-  } = weatherMockData;
+import * as utils from "./index.utils";
 
-  const weatherData = new WeatherData(t, h, p);
+describe("기상 현황 공유 옵저버 패턴", function () {
+  let currentObserverCount = 0;
+  const { temperatureGenerator, humidityGenerator, pressureGenerator } = utils;
+
+  const temperature = temperatureGenerator(25);
+  const humidity = humidityGenerator();
+  const pressure = pressureGenerator();
+
+  const weatherData = new WeatherData(temperature, humidity, pressure);
 
   it("날씨 데이터 초기값 확인", function () {
-    expect(weatherData.observersCount).to.equal(0);
-    expect(weatherData.temperature).to.equal(t);
-    expect(weatherData.humidity).to.equal(h);
-    expect(weatherData.pressure).to.equal(p);
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+    expect(weatherData.temperature).to.equal(temperature);
+    expect(weatherData.humidity).to.equal(humidity);
+    expect(weatherData.pressure).to.equal(pressure);
+  });
+
+  let currentConditionDisplay;
+  let statisticDisplay;
+  let forecastDisplay;
+
+  it("날씨 중계기 연결(옵저버)", function () {
+    currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+    currentObserverCount++;
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+    statisticDisplay = new StatisticDisplay(weatherData);
+    currentObserverCount++;
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+    forecastDisplay = new ForecastDisplay(weatherData);
+    currentObserverCount++;
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+  });
+
+  it("날씨 데이터 갱신", function () {
+    const temperature = temperatureGenerator(25);
+    const humidity = humidityGenerator();
+    const pressure = pressureGenerator();
+
+    weatherData.setMeasurements(temperature, humidity, pressure);
+
+    expect(weatherData.temperature).to.equal(temperature);
+    expect(weatherData.humidity).to.equal(humidity);
+    expect(weatherData.pressure).to.equal(pressure);
+  });
+
+  it("날씨 중계기 값 확인", function () {
+    expect(currentConditionDisplay.display).to.equal(
+      "currentConditionDisplay: Formatted message"
+    );
+    expect(statisticDisplay.display).to.equal(
+      "statisticDisplay: Formatted message"
+    );
+    expect(forecastDisplay.display).to.equal(
+      "forecastDisplay: Formatted message"
+    );
+  });
+
+  it("날씨 중계기 제거", function () {
+    weatherData.removeObserver(currentConditionDisplay);
+    currentObserverCount--;
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+  });
+
+  it("날씨 데이터 갱신", function () {
+    const temperature = temperatureGenerator(25);
+    const humidity = humidityGenerator();
+    const pressure = pressureGenerator();
+
+    weatherData.setMeasurements(temperature, humidity, pressure);
+
+    expect(weatherData.temperature).to.equal(temperature);
+    expect(weatherData.humidity).to.equal(humidity);
+    expect(weatherData.pressure).to.equal(pressure);
+  });
+
+  it("날씨 중계기 값 확인", function () {
+    expect(currentConditionDisplay.display).to.equal(
+      "currentConditionDisplay: Formatted message"
+    );
+    expect(statisticDisplay.display).to.equal(
+      "statisticDisplay: Formatted message"
+    );
+    expect(forecastDisplay.display).to.equal(
+      "forecastDisplay: Formatted message"
+    );
+  });
+
+  it("날씨 중계기 제거 중복 요청", function () {
+    weatherData.removeObserver(currentConditionDisplay);
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+  });
+
+  it("날씨 중계기 제거", function () {
+    weatherData.removeObserver(statisticDisplay);
+    currentObserverCount--;
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+    weatherData.removeObserver(forecastDisplay);
+    currentObserverCount--;
+    expect(weatherData.observersCount).to.equal(currentObserverCount);
+  });
+
+  it("날씨 데이터 갱신", function () {
+    const temperature = temperatureGenerator(25);
+    const humidity = humidityGenerator();
+    const pressure = pressureGenerator();
+
+    weatherData.setMeasurements(temperature, humidity, pressure);
+
+    expect(weatherData.temperature).to.equal(temperature);
+    expect(weatherData.humidity).to.equal(humidity);
+    expect(weatherData.pressure).to.equal(pressure);
   });
 });
