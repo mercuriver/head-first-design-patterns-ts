e6ac235134406a875ed8da5c4e499c1a41cd552d
mercuriver <mercuriver@gmail.com>
2022-10-07 22:28:39 +0900

    feat(observer): Display 추상 클래스 구현

--- patterns/observer/Display.ts
+++ patterns/observer/Display.ts
@@ -3,9 +3,42 @@ import DisplayElement from "./interfaces/DisplayElement";
 import WeatherData from "./WeatherData";
 
 abstract class Display implements Observer, DisplayElement {
-  constructor(weatherData: WeatherData) {}
-  display(): void {}
-  update(temp: number, humidity: number, pressure: number): void {}
+  #weatherData: WeatherData;
+  #temperature: number;
+  #humidity: number;
+  #pressure: number;
+
+  constructor(weatherData: WeatherData) {
+    this.#weatherData = weatherData;
+    this.#weatherData.registerObserver(this);
+  }
+
+  leave(): void {
+    this.#weatherData.removeObserver(this);
+  }
+
+  get temperature(): string {
+    return this.#temperature.toString();
+  }
+
+  get humidity(): string {
+    return this.#humidity.toString();
+  }
+
+  get pressure(): string {
+    return this.#pressure.toString();
+  }
+
+  update(temp: number, humidity: number, pressure: number): void {
+    this.#temperature = temp;
+    this.#humidity = humidity;
+    this.#pressure = pressure;
+    this.display();
+  }
+
+  display(): string {
+    return "설정된 표현 방식이 없습니다.";
+  }
 }
 
 export default Display;
