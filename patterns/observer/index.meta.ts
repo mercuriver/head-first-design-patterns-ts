b2e49f63ec59ec7e02b513788a6154c5638fe2c3
mercuriver <mercuriver@gmail.com>
2022-10-07 22:28:15 +0900

    feat(observer): 메시지 meta 변경

--- patterns/observer/index.meta.ts
+++ patterns/observer/index.meta.ts
@@ -1,11 +1,8 @@
-const weatherMockData: {
-  temperature: number[];
-  humidity: number[];
-  pressure: number[];
-} = {
-  temperature: [26],
-  humidity: [64],
-  pressure: [1013.25], // 단위: hPa
+const displayTemplate = {
+  currentCodition: "현재 상태: 온도 %0F, 습도: %1%",
+  statistic: "평균 온도: %0F / 최고 온도: %1F / 최저 온도: %2F",
+  posiviteForecast: "기상 예보: 날씨가 좋아지고 있습니다...",
+  negativeForecast: "기상 예보: 날씨가 나빠지고 있습니다!",
 };
 
-export default weatherMockData;
+export { displayTemplate };
