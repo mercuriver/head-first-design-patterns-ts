1540f46b225fe2896b590be13d8e03a0b0a6c2e9
mercuriver <mercuriver@gmail.com>
2022-10-07 22:30:45 +0900

    feat(observer): 현재 날씨 표현자 구현

--- patterns/observer/CurrentConditionDisplay.ts
+++ patterns/observer/CurrentConditionDisplay.ts
@@ -1,5 +1,19 @@
 import Display from "./Display";
 
-class CurrentConditionDisplay extends Display {}
+import { printf } from "./index.utils";
+import { displayTemplate } from "./index.meta";
+
+class CurrentConditionDisplay extends Display {
+  display(): string {
+    const message = printf(
+      displayTemplate.currentCodition,
+      this.temperature,
+      this.humidity,
+      this.pressure
+    );
+    console.log(message);
+    return message;
+  }
+}
 
 export default CurrentConditionDisplay;
