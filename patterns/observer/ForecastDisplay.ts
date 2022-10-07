ef2798a805f70eea67b6879aa843fb9d2ec734a9
mercuriver <mercuriver@gmail.com>
2022-10-07 22:30:58 +0900

    feat(observer): 기상 예측 표현자 구현

--- patterns/observer/ForecastDisplay.ts
+++ patterns/observer/ForecastDisplay.ts
@@ -1,5 +1,28 @@
 import Display from "./Display";
+import { displayTemplate } from "./index.meta";
+class ForecastDisplay extends Display {
+  prevTemp: number;
+  isPositive: boolean;
 
-class ForecastDisplay extends Display {}
+  constructor(weatherData) {
+    super(weatherData);
+    this.isPositive = true;
+    this.prevTemp = -Infinity;
+  }
+
+  update(temp: number, humidity: number, pressure: number): void {
+    super.update(temp, humidity, pressure);
+    this.isPositive = this.prevTemp < temp;
+    this.prevTemp = temp;
+  }
+
+  display(): string {
+    const message = this.isPositive
+      ? displayTemplate.posiviteForecast
+      : displayTemplate.negativeForecast;
+    console.log(message);
+    return message;
+  }
+}
 
 export default ForecastDisplay;
