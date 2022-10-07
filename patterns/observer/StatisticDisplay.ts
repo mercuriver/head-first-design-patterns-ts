3ac9969746198abe2ba732d170b68e9f9646ca03
mercuriver <mercuriver@gmail.com>
2022-10-07 22:31:05 +0900

    feat(observer): 기상 통계 표현자 구현

--- patterns/observer/StatisticDisplay.ts
+++ patterns/observer/StatisticDisplay.ts
@@ -1,5 +1,42 @@
 import Display from "./Display";
 
-class StatisticDisplay extends Display {}
+import { printf } from "./index.utils";
+import { displayTemplate } from "./index.meta";
+class StatisticDisplay extends Display {
+  // average 구현 생략
+  // #highest;
+  // #lowest;
+  highest: number;
+  lowest: number;
+  // classfield(#)을 사용할 경우 Display 추상 클레스에서 display() 호출 시 에러 발생, 확인 필요
+
+  constructor(weatherData) {
+    console.log("####### constructor");
+    super(weatherData);
+    this.highest = -99;
+    this.lowest = 99;
+    console.log("####### 3", this.highest, this.lowest);
+  }
+
+  update(temp: number, humidity: number, pressure: number): void {
+    console.log("####### 1", temp, this.highest, this.lowest);
+    this.highest = Math.max(this.highest, temp);
+    this.lowest = Math.min(this.lowest, temp);
+    console.log("####### 2", temp, this.highest, this.lowest);
+    super.update(temp, humidity, pressure);
+  }
+
+  display(): string {
+    console.log("#######", this.highest, this.lowest);
+    const message = printf(
+      displayTemplate.statistic,
+      this.temperature,
+      this.highest.toString(),
+      this.lowest.toString()
+    );
+    console.log(message);
+    return message;
+  }
+}
 
 export default StatisticDisplay;
