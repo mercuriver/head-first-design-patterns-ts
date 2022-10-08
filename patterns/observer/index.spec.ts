26636f279b8a407075312f411c42b2d460086b85
mercuriver <mercuriver@gmail.com>
2022-10-09 00:22:47 +0900

    test(observer): 표현력 강화를 위한 테스트 케이스 정리

--- patterns/observer/index.spec.ts
+++ patterns/observer/index.spec.ts
@@ -25,15 +25,19 @@ const printWithValues =
       pressure.toString()
     );
 
-describe("util 테스트", function () {
+describe(`util 테스트`, function () {
   const { printf } = utils;
-  expect(printf("%0 %1 %2")).to.equal("");
-  expect(printf("%0 %1 %2", "A", "B", "C")).to.equal("A B C");
-  expect(printf("%0 %1 %1", "A", "B", "C")).to.equal("A B B");
-  expect(printf("%2 %2 %1", "A", "B", "C")).to.equal("C C B");
+
+  it(`문자열 포멧 함수 확인`, function () {
+    expect(printf("%0 %1 %2")).to.equal("");
+    expect(printf("%0 %1 %2", "A", "B", "C")).to.equal("A B C");
+    expect(printf("%0 %1 %1", "A", "B", "C")).to.equal("A B B");
+    expect(printf("%2 %2 %1", "A", "B", "C")).to.equal("C C B");
+  });
 });
 
-describe("기상 현황 공유 옵저버 패턴", function () {
+describe(`기상 현황 공유 옵저버 패턴`, function () {
+  let seq = 1;
   let currentObserverCount = 0;
 
   const firstTemp = 27;
@@ -53,12 +57,12 @@ describe("기상 현황 공유 옵저버 패턴", function () {
 
   const weatherData = new WeatherData(weatherDataProps);
 
-  it("날씨 데이터 초기값 확인", function () {
+  it(`[${seq++}] 날씨 데이터 초기값 확인`, function () {
     expect(weatherData.observersCount).to.equal(currentObserverCount);
     expect(weatherData.data).to.eql(weatherDataProps);
   });
 
-  it("날씨 중계기 연결(옵저버)", function () {
+  it(`[${seq++}] 날씨 중계기 연결(옵저버)`, function () {
     currentConditionDisplay = new CurrentConditionDisplay(weatherData);
     currentObserverCount++;
     expect(weatherData.observersCount).to.equal(currentObserverCount);
@@ -70,7 +74,7 @@ describe("기상 현황 공유 옵저버 패턴", function () {
     expect(weatherData.observersCount).to.equal(currentObserverCount);
   });
 
-  it("날씨 중계기 값 확인", function () {
+  it(`[${seq++}] 날씨 중계기 값 확인`, function () {
     expect(currentConditionDisplay.display()).to.equal(
       printWithValues(firstTemp, humidity, pressure)(currentCodition)
     );
@@ -80,52 +84,55 @@ describe("기상 현황 공유 옵저버 패턴", function () {
     expect(forecastDisplay.display()).to.equal(defaultForecast);
   });
 
-  it("날씨 데이터 갱신: [날씨 추워짐]", function () {
+  it(`[${seq++}] 날씨 데이터 갱신: [날씨 추워짐]`, function () {
     weatherDataProps.temperature = secondTemp;
     weatherData.setMeasurements(weatherDataProps);
     expect(weatherData.data).to.eql(weatherDataProps);
   });
 
-  it("날씨 중계기 값 확인: [날씨 추워짐]", function () {
-    const printer = printWithValues(secondTemp, humidity, pressure);
-    expect(currentConditionDisplay.display()).to.equal(
-      printer(currentCodition)
-    );
+  const cache = printWithValues(
+    secondTemp,
+    humidity,
+    pressure
+  )(currentCodition);
+  it(`[${seq++}] 날씨 중계기 값 확인: [날씨 추워짐]`, function () {
+    expect(currentConditionDisplay.display()).to.equal(cache);
     expect(statisticDisplay.display()).to.equal(
       printWithValues(secondTemp, firstTemp, secondTemp)(statistic)
     );
     expect(forecastDisplay.display()).to.equal(negativeForecast);
   });
 
-  it("날씨 중계기 제거", function () {
+  it(`[${seq++}] 날씨 중계기 제거`, function () {
     weatherData.removeObserver(currentConditionDisplay);
     currentObserverCount--;
     expect(weatherData.observersCount).to.equal(currentObserverCount);
   });
 
-  it("날씨 중계기 값 확인", function () {
-    expect(currentConditionDisplay.display).to.equal(
-      "currentConditionDisplay: Formatted message"
-    );
-    expect(statisticDisplay.display).to.equal(
-      "statisticDisplay: Formatted message"
-    );
-    expect(forecastDisplay.display).to.equal(
-      "forecastDisplay: Formatted message"
-    );
+  it(`[${seq++}] 날씨 데이터 갱신: [날씨 따듯해짐]`, function () {
+    weatherDataProps.temperature = firstTemp;
+    weatherData.setMeasurements(weatherDataProps);
+    expect(weatherData.data).to.eql(weatherDataProps);
   });
 
-  it("날씨 중계기 제거 중복 요청", function () {
+  it(`[${seq++}] 날씨 데이터 갱신: [날씨 따듯해짐]`, function () {
+    expect(statisticDisplay.display()).to.equal(
+      printWithValues(firstTemp, firstTemp, secondTemp)(statistic)
+    );
+    expect(forecastDisplay.display()).to.equal(posiviteForecast);
+  });
+  it(`[${seq++}] 날씨 중계기 값 확인: 삭제된 diplay엔 이전 값 출력`, function () {
+    expect(currentConditionDisplay.display()).to.equal(cache);
+  });
+
+  it(`[${seq++}] 날씨 중계기 제거 중복 요청`, function () {
     weatherData.removeObserver(currentConditionDisplay);
     expect(weatherData.observersCount).to.equal(currentObserverCount);
   });
 
-  it("날씨 중계기 제거", function () {
+  it(`[${seq++}] 모든 날씨 중계기 제거 후 observersCount 0 확인`, function () {
     weatherData.removeObserver(statisticDisplay);
-    currentObserverCount--;
-    expect(weatherData.observersCount).to.equal(currentObserverCount);
     weatherData.removeObserver(forecastDisplay);
-    currentObserverCount--;
-    expect(weatherData.observersCount).to.equal(currentObserverCount);
+    expect(weatherData.observersCount).to.equal(0);
   });
 });
