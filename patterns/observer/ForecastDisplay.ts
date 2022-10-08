12817f21a2be167f79ca45ac6f9f51e68f10894b
mercuriver <mercuriver@gmail.com>
2022-10-08 18:31:53 +0900

    feat(observer): 옵저버 초기화 로직 변경에 따른 Display 값 초기화 절차 개선

--- patterns/observer/ForecastDisplay.ts
+++ patterns/observer/ForecastDisplay.ts
@@ -2,24 +2,32 @@ import Display from "./Display";
 import { displayTemplate } from "./index.meta";
 class ForecastDisplay extends Display {
   prevTemp: number;
-  isPositive: boolean;
+  temperatureGap: number;
 
   constructor(weatherData) {
     super(weatherData);
-    this.isPositive = true;
-    this.prevTemp = -Infinity;
+    this.temperatureGap = 0;
+    this.prevTemp = this.temperature;
   }
 
-    super.update(temp, humidity, pressure);
-    this.isPositive = this.prevTemp < temp;
-    this.prevTemp = temp;
   update(data): void {
+    const { temperature } = data;
+    super.update(data);
+    this.temperatureGap = this.prevTemp - temperature;
+    this.prevTemp = temperature;
+  }
+
+  getForecastMessage(): string {
+    if (this.temperatureGap !== 0) {
+      return this.temperatureGap > 0
+        ? displayTemplate.posiviteForecast
+        : displayTemplate.negativeForecast;
+    }
+    return displayTemplate.defaultForecast;
   }
 
   display(): string {
-    const message = this.isPositive
-      ? displayTemplate.posiviteForecast
-      : displayTemplate.negativeForecast;
+    const message = this.getForecastMessage();
     console.log(message);
     return message;
   }
--- patterns/observer/StatisticDisplay.ts
+++ patterns/observer/StatisticDisplay.ts
@@ -2,6 +2,7 @@ import Display from "./Display";
 
 import { printf } from "./index.utils";
 import { displayTemplate } from "./index.meta";
+
 class StatisticDisplay extends Display {
   // average 구현 생략
   // #highest;
@@ -11,19 +12,17 @@ class StatisticDisplay extends Display {
   // classfield(#)을 사용할 경우 Display 추상 클레스에서 display() 호출 시 에러 발생, 확인 필요
 
   constructor(weatherData) {
-    console.log("####### constructor");
     super(weatherData);
-    this.highest = -99;
-    this.lowest = 99;
-    console.log("####### 3", this.highest, this.lowest);
+
+    this.highest = this.temperature;
+    this.lowest = this.temperature;
   }
 
-    console.log("####### 1", temp, this.highest, this.lowest);
-    this.highest = Math.max(this.highest, temp);
-    this.lowest = Math.min(this.lowest, temp);
-    console.log("####### 2", temp, this.highest, this.lowest);
-    super.update(temp, humidity, pressure);
   update(data) {
+    const { temperature } = data;
+    this.highest = Math.max(this.highest, temperature);
+    this.lowest = Math.min(this.lowest, temperature);
+    super.update(data);
   }
 
   display() {
