19cac1fda43ae4b88e5b5e8c70c4d03f4565b47b
mercuriver <mercuriver@gmail.com>
2022-10-10 21:22:19 +0900

    feat(observer): Display 초기화 메시지 추가

--- patterns/observer/index.meta.ts
+++ patterns/observer/index.meta.ts
@@ -1,4 +1,5 @@
 const displayTemplate = {
+  defaultMessage: "초기화 중...",
   currentCodition: "현재 상태: 온도 %0F, 습도: %1%",
   statistic: "평균 온도: %0F / 최고 온도: %1F / 최저 온도: %2F",
   defaultForecast: "기상 예보: 아직은 앞날을 알 수 없지요.",
