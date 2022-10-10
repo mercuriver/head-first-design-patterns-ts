67cbc091569814cdfcf30bb4888e83c64108206b
mercuriver <mercuriver@gmail.com>
2022-10-10 21:33:54 +0900

    refactor(observer): pull 타입 display 내 클래스필드 적용

--- patterns/observer/pullMechanism/StatisticDisplay.ts
+++ patterns/observer/pullMechanism/StatisticDisplay.ts
@@ -4,19 +4,19 @@ import { printf } from "../index.utils";
 import { displayTemplate } from "../index.meta";
 
 class StatisticDisplay extends Display {
-  highest: number;
-  lowest: number;
+  #highest: number;
+  #lowest: number;
 
   constructor(weatherData) {
     super(weatherData);
-    this.highest = -Infinity;
-    this.lowest = Infinity;
+    this.#highest = -Infinity;
+    this.#lowest = Infinity;
   }
 
   update() {
     const temperature = this.weatherData.temperature;
-    this.highest = Math.max(this.highest, temperature);
-    this.lowest = Math.min(this.lowest, temperature);
+    this.#highest = Math.max(this.#highest, temperature);
+    this.#lowest = Math.min(this.#lowest, temperature);
     super.update();
   }
 
@@ -24,8 +24,8 @@ class StatisticDisplay extends Display {
     this.message = printf(
       displayTemplate.statistic,
       this.temperature.toString(),
-      this.highest.toString(),
-      this.lowest.toString()
+      this.#highest.toString(),
+      this.#lowest.toString()
     );
   }
 }
