0d83bd8bbb7fabe1a53477deb2b72c3e2bbeb5a0
mercuriver <mercuriver@gmail.com>
2022-10-13 22:01:55 +0900

    feat(decorator): 음료 가격 데이터 구성

---
+++ patterns/decorator/index.meta.ts
@@ -0,0 +1,12 @@
+const COST = {
+  HOUSE_BLEND: 0.89,
+  DARK_ROAST: 0.99,
+  DECAFFEINE: 1.05,
+  ESPRESSO: 1.99,
+  MILK: 0.1,
+  MOCHA: 0.2,
+  SOY: 0.15,
+  WHIPPED_CREAM: 0.1,
+} as const;
+
+export { COST };
