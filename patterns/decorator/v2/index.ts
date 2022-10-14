9a20af4154b92158e16a816493d2e4cb228c0db0
Mercuriver <mercuriver@gmail.com>
2022-10-15 01:28:04 +0900

    feat(decorator): 해괴한 데코레이터 음료 클래스 생성기 적용

--- patterns/decorator/v2/index.ts
+++ patterns/decorator/v2/index.ts
@@ -1,4 +1,12 @@
-export { default as HouseBlend } from "./HouseBlend";
-export { default as DarkRoast } from "./DarkRoast";
-export { default as Milk } from "./Milk";
-export { default as Soy } from "./Soy";
+import BeverageGenerator from "./BeverageGenerator";
+import { COST } from "../index.meta";
+
+export const Espresso = BeverageGenerator("에스프레소", COST.ESPRESSO);
+export const Decaffeine = BeverageGenerator("디카페인", COST.DECAFFEINE);
+export const DarkRoast = BeverageGenerator("다크 로스트", COST.DARK_ROAST);
+export const HouseBlend = BeverageGenerator("하우스 블랜드", COST.HOUSE_BLEND);
+
+export const Mocha = BeverageGenerator("모카", COST.MOCHA);
+export const Soy = BeverageGenerator("두유", COST.SOY);
+export const Milk = BeverageGenerator("우유", COST.MILK);
+export const WhippedCream = BeverageGenerator("휘핑 크림", COST.WHIPPED_CREAM);
