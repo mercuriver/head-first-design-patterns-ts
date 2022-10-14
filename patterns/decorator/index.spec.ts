22e8e8f523a02abed4d2d4fc8e7c8351c8b9c3e8
mercuriver <mercuriver@gmail.com>
2022-10-14 23:28:40 +0900

    test(decorator): 기본 테스트 케이스 추가

--- patterns/decorator/index.spec.ts
+++ patterns/decorator/index.spec.ts
@@ -1,7 +1,14 @@
 import { expect } from "chai";
 
+import { COST } from "./index.meta";
+
+import { HouseBlend } from "./beverage";
+import { Milk } from "./extra";
+
 describe(`데코레이터 패턴 테스트`, function () {
-  it(`문자열 포멧 함수 확인`, function () {
-    expect("").to.equal("");
+  it(`기본적인 음료 테스트`, function () {
+    const milkCoffee = new Milk(new HouseBlend());
+
+    expect(milkCoffee.cost()).to.equal(COST.HOUSE_BLEND + COST.MILK);
   });
 });
