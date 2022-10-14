23e870a1fd4012011216e2a622493b2223518e07
mercuriver <mercuriver@gmail.com>
2022-10-14 23:30:41 +0900

    feat(decorator): 음료 클래스 cost 구현 수정

--- patterns/decorator/beverage/HouseBlend.ts
+++ patterns/decorator/beverage/HouseBlend.ts
@@ -2,8 +2,6 @@ import Beverage from "../core/Beverage";
 import { COST } from "../index.meta";
 
 class HouseBlend extends Beverage {
-  // Todo: 전달 받은 음료 cost 합산
-
   cost(): number {
     return COST.HOUSE_BLEND;
   }
--- patterns/decorator/extra/Milk.ts
+++ patterns/decorator/extra/Milk.ts
@@ -2,10 +2,8 @@ import CondimentDecorator from "../core/CondimentDecorator";
 import { COST } from "../index.meta";
 
 class Milk extends CondimentDecorator {
-  // Todo: 전달 받은 음료 cost 합산
-
   cost(): number {
-    return COST.MILK;
+    return super.cost() + COST.MILK;
   }
 }
 
