3b45d6f3cbeb4f73532558058a927fe26c33389a
Mercuriver <mercuriver@gmail.com>
2022-10-16 00:05:15 +0900

    test(decorator): 데코레이터 활용 케이스 별 테스트 케이스 작성

--- patterns/decorator/v2/index.spec.ts
+++ patterns/decorator/v2/index.spec.ts
@@ -8,9 +8,21 @@ describe(`이상한 데코레이터 패턴 테스트`, function () {
   it(`기본적인 음료 테스트`, function () {
     const milkCoffee = new Milk(new HouseBlend());
     expect(milkCoffee.cost).to.equal(COST.HOUSE_BLEND + COST.MILK);
-    const doNotOrder = new DarkRoast(new Soy(new Milk(new HouseBlend())));
+    // const doNotOrder = new DarkRoast(new Soy(new Milk(new HouseBlend())));
+
+    const doNotOrder = new DarkRoast().add(Soy).add(Milk).add(HouseBlend);
+
     expect(doNotOrder.cost).to.equal(
       COST.HOUSE_BLEND + COST.MILK + COST.DARK_ROAST + COST.SOY
     );
+
+    const somethingWrong = new DarkRoast()
+      .add(DarkRoast)
+      .add(DarkRoast)
+      .add(DarkRoast)
+      .add(DarkRoast)
+      .add(DarkRoast);
+
+    expect(somethingWrong.cost).to.equal(COST.DARK_ROAST * 6);
   });
 });
