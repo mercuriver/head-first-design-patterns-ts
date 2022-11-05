359c3a4fc27e9151994a707d883e3d78eb08113d
mercuriver <mercuriver@gmail.com>
2022-11-05 22:20:50 +0900

    feat(factory method): Pizza 타입 별 키, 타입 메타 추가

--- patterns/factory/index.meta.ts
+++ patterns/factory/index.meta.ts
@@ -1,10 +1,30 @@
-const pizzaStyle = {
-  NY: "뉴욕 스타일",
-  Chicago: "시카고 스타일",
+const PIZZA_STYLE = {
+  NEW_YORK: "NY",
+  CHICAGO: "Chicago",
+} as const;
+
+type PizzaStyle = typeof PIZZA_STYLE[keyof typeof PIZZA_STYLE];
+
+const PIZZA_TYPE = {
+  CHEESE: "Cheese",
+  VEGGIE: "Veggie",
+  CLAM: "Clam",
+  PEPPERONI: "Pepperoni",
+} as const;
+
+type PizzeType = typeof PIZZA_TYPE[keyof typeof PIZZA_TYPE];
+
+const pizzaStyleName = {
+  [PIZZA_STYLE.NEW_YORK]: "뉴욕 스타일",
+  [PIZZA_STYLE.CHICAGO]: "시카고 스타일",
 };
 
-const pizzaType = {
-  Cheese: "치즈 피자",
+const pizzaTypeName = {
+  [PIZZA_TYPE.CHEESE]: "치즈",
+  [PIZZA_TYPE.VEGGIE]: "베지",
+  [PIZZA_TYPE.CLAM]: "크램",
+  [PIZZA_TYPE.PEPPERONI]: "페퍼로니",
 };
 
-export { pizzaStyle, pizzaType };
+export type { PizzaStyle, PizzeType };
+export { PIZZA_STYLE, pizzaStyleName, PIZZA_TYPE, pizzaTypeName };
