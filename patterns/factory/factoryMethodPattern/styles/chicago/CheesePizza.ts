0d64b78f5c3ae522d0c591b150c1dd2ccce106eb
mercuriver <mercuriver@gmail.com>
2022-11-05 22:21:51 +0900

    feat(factory method): style 별 pizza 클래스 작성

---
+++ patterns/factory/factoryMethodPattern/styles/chicago/CheesePizza.ts
@@ -0,0 +1,10 @@
+import Pizza from "../../Pizza";
+
+class CheesePizza extends Pizza {
+  constructor() {
+    super();
+    // super({ name: "", dough: "", sauce: "", toppings: "" });
+  }
+}
+
+export default CheesePizza;
