3a747541a93f81c01e6e189f285246b740700102
mercuriver <mercuriver@gmail.com>
2022-11-07 14:45:18 +0900

    feat(factory method): 시카고 스타일 피자 기본 클래스 작성

---
+++ patterns/factory/factoryMethodPattern/styles/chicago/ChicagoPizza.ts
@@ -0,0 +1,24 @@
+import Pizza from "../../Pizza";
+import type { PizzaProps } from "../../Pizza";
+
+import type { Optional } from "../../../index.meta";
+import { pizzaStyleName } from "../../../index.meta";
+
+type Props = Optional<PizzaProps, "dough" | "sauce">;
+
+class ChicagoPizza extends Pizza {
+  constructor({ name, dough, sauce, toppings }: Props) {
+    super({
+      name: `${pizzaStyleName.Chicago} ${name}`,
+      dough: dough || "아주 두꺼운 크러스트 도우",
+      sauce: sauce || "플럼토마토 소스",
+      toppings,
+    });
+  }
+
+  cut(): void {
+    console.log("네모난 모양으로 피자 자르기");
+  }
+}
+
+export default ChicagoPizza;
