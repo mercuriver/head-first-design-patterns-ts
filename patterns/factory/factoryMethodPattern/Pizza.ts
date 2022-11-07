1a8daf6a5dbf767b28457f7d02998d9fa3214a31
mercuriver <mercuriver@gmail.com>
2022-11-07 14:44:58 +0900

    feat(factory method): 기본 피자 추상 클래스 상세 내역 구현

--- patterns/factory/factoryMethodPattern/Pizza.ts
+++ patterns/factory/factoryMethodPattern/Pizza.ts
@@ -20,15 +20,19 @@ abstract class Pizza {
 
   prepare(): void {
     console.log(`준비 중: ${this.#name}`);
-    console.log("도우를 돌리는 중...");
-    console.log("소스를 뿌리는 중...");
-    console.log("토핑를 올리는 중...");
-    console.log(this.#toppings.join("   "));
+    console.log(`${this.#dough} 를 돌리는 중...`);
+    console.log(`${this.#sauce}를 뿌리는 중...`);
+    console.log(`토핑를 올리는 중...\n\t${this.#toppings.join("\n\t")}`);
   }
-
-  bake(): void {}
-  cut(): void {}
-  box(): void {}
+  bake(): void {
+    console.log("175도에서 25분 간 굽기");
+  }
+  cut(): void {
+    console.log("피자를 사선으로 자르기");
+  }
+  box(): void {
+    console.log("상자에 피자 담기");
+  }
 
   get name(): string {
     return this.#name;
