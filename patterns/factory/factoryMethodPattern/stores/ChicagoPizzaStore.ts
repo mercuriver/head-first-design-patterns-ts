779ee14569772ba38a9d464d7da8a8ef2418db71
mercuriver <mercuriver@gmail.com>
2022-11-08 19:55:09 +0900

    feat(factory method): NY/Chicago store 작성

---
+++ patterns/factory/factoryMethodPattern/stores/ChicagoPizzaStore.ts
@@ -0,0 +1,30 @@
+import Pizza from "../Pizza";
+import PizzaStore from "../PizzaStore";
+import {
+  ChicagoStyleCheesePizza,
+  ChicagoStyleClamPizza,
+  ChicagoStyleVeggiePizza,
+  ChicagoStylePepperoniPizza,
+} from "../styles/chicago/index";
+
+import { PIZZA_TYPE, type PizzaType } from "../../index.meta";
+
+class ChicagoPizzaStore extends PizzaStore {
+  createPizza(type: PizzaType): Pizza {
+    if (type === PIZZA_TYPE.CHEESE) {
+      return new ChicagoStyleCheesePizza();
+    }
+    if (type === PIZZA_TYPE.CLAM) {
+      return new ChicagoStyleClamPizza();
+    }
+    if (type === PIZZA_TYPE.VEGGIE) {
+      return new ChicagoStyleVeggiePizza();
+    }
+    if (type === PIZZA_TYPE.PEPPERONI) {
+      return new ChicagoStylePepperoniPizza();
+    }
+    return null;
+  }
+}
+
+export default ChicagoPizzaStore;
--- patterns/factory/factoryMethodPattern/stores/NYPizzaStore.ts
+++ patterns/factory/factoryMethodPattern/stores/NYPizzaStore.ts
@@ -1,10 +1,28 @@
 import Pizza from "../Pizza";
 import PizzaStore from "../PizzaStore";
+import {
+  NYStyleCheesePizza,
+  NYStyleClamPizza,
+  NYStyleVeggiePizza,
+  NYStylePepperoniPizza,
+} from "../styles/ny/index";
 
-import type { PizzaType } from "../../index.meta";
+import { PIZZA_TYPE, type PizzaType } from "../../index.meta";
 
 class NYPizzaStore extends PizzaStore {
   createPizza(type: PizzaType): Pizza {
+    if (type === PIZZA_TYPE.CHEESE) {
+      return new NYStyleCheesePizza();
+    }
+    if (type === PIZZA_TYPE.CLAM) {
+      return new NYStyleClamPizza();
+    }
+    if (type === PIZZA_TYPE.VEGGIE) {
+      return new NYStyleVeggiePizza();
+    }
+    if (type === PIZZA_TYPE.PEPPERONI) {
+      return new NYStylePepperoniPizza();
+    }
     return null;
   }
 }
