68c64c7bc978f172c6de584c1d1a94da7cefba9e
mercuriver <mercuriver@gmail.com>
2022-11-09 19:53:32 +0900

    refactor(factory): 공통 meta 정리

--- patterns/factory/index.meta.ts
+++ patterns/factory/index.meta.ts
@@ -1,3 +1,5 @@
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
 const PIZZA_STYLE = {
   NEW_YORK: "NY",
   CHICAGO: "Chicago",
@@ -14,5 +16,25 @@ const PIZZA_TYPE = {
 
 type PizzaType = typeof PIZZA_TYPE[keyof typeof PIZZA_TYPE];
 
-export type { PizzaStyle, PizzaType };
-export { PIZZA_STYLE, PIZZA_TYPE };
+const pizzaStyleName = {
+  [PIZZA_STYLE.NEW_YORK]: "뉴욕 스타일",
+  [PIZZA_STYLE.CHICAGO]: "시카고 스타일",
+};
+
+const pizzaTypeName = {
+  [PIZZA_STYLE.NEW_YORK]: {
+    [PIZZA_TYPE.CHEESE]: "소스와 치즈 피자",
+    [PIZZA_TYPE.VEGGIE]: "베지 피자",
+    [PIZZA_TYPE.CLAM]: "크램 피자",
+    [PIZZA_TYPE.PEPPERONI]: "페퍼로니 피자",
+  },
+  [PIZZA_STYLE.CHICAGO]: {
+    [PIZZA_TYPE.CHEESE]: "딥 디쉬 치즈 피자",
+    [PIZZA_TYPE.VEGGIE]: "베지 피자",
+    [PIZZA_TYPE.CLAM]: "크램 피자",
+    [PIZZA_TYPE.PEPPERONI]: "페퍼로니 피자",
+  },
+};
+
+export type { Optional, PizzaStyle, PizzaType };
+export { PIZZA_STYLE, pizzaStyleName, PIZZA_TYPE, pizzaTypeName };
