ce242d81f9307d116655868c6bcc342757c2eb6c
mercuriver <mercuriver@gmail.com>
2022-11-09 19:55:23 +0900

    test(abstract  factory): 추상 팩토리 패턴 예제 테스트 케이스 작성

--- patterns/factory/abstractFactoryPattern/index.spec.ts
+++ patterns/factory/abstractFactoryPattern/index.spec.ts
@@ -1,3 +1,46 @@
+import { expect } from "chai";
+
+import PizzaStore from "./pizzaStore/PizzaStore";
+import { NYPizzaStore, ChicagoPizzaStore } from "./pizzaStore";
+
+import {
+  PIZZA_STYLE,
+  PIZZA_TYPE,
+  pizzaStyleName,
+  pizzaTypeName,
+  type PizzaStyle,
+  type PizzaType,
+} from "../index.meta";
+
+const pizzaFactoryAssertion = (
+  store: PizzaStore,
+  style: PizzaStyle,
+  type: PizzaType
+): Chai.Assertion =>
+  expect(store.orderPizza(type).name).to.equal(
+    `${pizzaStyleName[style]} ${pizzaTypeName[style][type]}`
+  );
+
 describe(`[추상 팩토리 패턴] 테스트`, function () {
-  it(`기본적인 피자 생성 테스트`, function () {});
+  let seq = 1;
+
+  it(`[${seq++}] 뉴욕 피자 생성`, function () {
+    const nyStore = new NYPizzaStore();
+    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.CHEESE);
+    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.CLAM);
+    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.VEGGIE);
+    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.PEPPERONI);
+  });
+
+  it(`[${seq++}] 시카고 치즈 피자 생성`, function () {
+    const chicagoStore = new ChicagoPizzaStore();
+    pizzaFactoryAssertion(chicagoStore, PIZZA_STYLE.CHICAGO, PIZZA_TYPE.CHEESE);
+    pizzaFactoryAssertion(chicagoStore, PIZZA_STYLE.CHICAGO, PIZZA_TYPE.CLAM);
+    pizzaFactoryAssertion(chicagoStore, PIZZA_STYLE.CHICAGO, PIZZA_TYPE.VEGGIE);
+    pizzaFactoryAssertion(
+      chicagoStore,
+      PIZZA_STYLE.CHICAGO,
+      PIZZA_TYPE.PEPPERONI
+    );
+  });
 });
