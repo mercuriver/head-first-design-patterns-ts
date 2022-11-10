a229eba863567fcfaac1349712f0dfaca4d4ed79
mercuriver <mercuriver@gmail.com>
2022-11-10 18:16:43 +0900

    feat(simple factory): 전용 로컬 메타 데이터 추가

---
+++ patterns/factory/simpleFactory/index.meta.ts
@@ -0,0 +1,10 @@
+import { PIZZA_TYPE } from "../index.meta";
+
+const pizzaName = {
+  [PIZZA_TYPE.CHEESE]: "치즈 피자",
+  [PIZZA_TYPE.VEGGIE]: "베지 피자",
+  [PIZZA_TYPE.CLAM]: "크램 피자",
+  [PIZZA_TYPE.PEPPERONI]: "페퍼로니 피자",
+};
+
+export { PIZZA_TYPE, pizzaName };
