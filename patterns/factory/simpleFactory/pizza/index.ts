448ef20b307580efe1d7091c7fb22152a1ebb2b3
mercuriver <mercuriver@gmail.com>
2022-11-10 14:58:34 +0900

    feat(simple factory): pizza 서브 클래스 export 정리

---
+++ patterns/factory/simpleFactory/pizza/index.ts
@@ -0,0 +1,4 @@
+export { default as CheesePizza } from "./CheesePizza";
+export { default as ClamPizza } from "./ClamPizza";
+export { default as VeggiePizza } from "./VeggiePizza";
+export { default as PepperoniPizza } from "./PepperoniPizza";
