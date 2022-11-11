097ea0b3594b3bc884f9349c3daf114f145c7ef3
mercuriver <mercuriver@gmail.com>
2022-11-11 17:40:07 +0900

    test(singleton): 테스트 케이스 작성

---
+++ patterns/singleton/basics/index.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from "chai";
+
+import SingletonChocolateBoiler from "./SingletonChocolateBoiler";
+
+describe(`[기본 싱글턴] 테스트`, function () {
+  it(`싱글턴, 동일 인스턴스 체크`, function () {
+    const firstBoiler = SingletonChocolateBoiler.getInstance();
+    const secondBoiler = SingletonChocolateBoiler.getInstance();
+
+    firstBoiler.fill();
+    secondBoiler.fill();
+    firstBoiler.boil();
+    secondBoiler.boil();
+    secondBoiler.drain();
+    firstBoiler.drain();
+
+    expect(firstBoiler).to.equal(secondBoiler);
+  });
+});
