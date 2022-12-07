5dcc8275c52d88ce8c4f5b5794c95172ccc68c59
mercuriver <mercuriver@gmail.com>
2022-12-07 12:03:21 +0900

    refactor(factory): 테스트 코드, assertion 코드 정리 (커리 적용)

--- patterns/factory/abstractFactoryPattern/index.spec.ts
+++ patterns/factory/abstractFactoryPattern/index.spec.ts
@@ -17,39 +17,37 @@ import {
   type PizzaType,
 } from "../index.meta";
 
-const pizzaFactoryAssertion = (
-  store: PizzaStore,
-  style: PizzaStyle,
-  type: PizzaType
-): Chai.Assertion =>
-  expect(store.orderPizza(type).name).to.equal(
-    `${pizzaStyleName[style]} ${pizzaTypeName[style][type]}`
-  );
+const buildAssertion =
+  (factory: PizzaIngredientFactory, style: PizzaStyle) =>
+  (type: PizzaType): Chai.Assertion => {
+    const store = new PizzaStore();
+    return expect(store.orderPizza(factory, type).name).to.equal(
+      `${pizzaStyleName[style]} ${pizzaTypeName[style][type]}`
+    );
+  };
 
 describe(`[추상 팩토리 패턴] 테스트`, function () {
   let seq = 1;
 
   it(`[${seq++}] 뉴욕 피자 생성`, function () {
-    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.CHEESE);
-    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.CLAM);
-    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.VEGGIE);
-    pizzaFactoryAssertion(nyStore, PIZZA_STYLE.NEW_YORK, PIZZA_TYPE.PEPPERONI);
     const ingredientFactory: PizzaIngredientFactory =
       new NYPizzaIngredientFactory();
 
+    const assertion = buildAssertion(ingredientFactory, PIZZA_STYLE.NEW_YORK);
+    assertion(PIZZA_TYPE.CHEESE);
+    assertion(PIZZA_TYPE.CLAM);
+    assertion(PIZZA_TYPE.VEGGIE);
+    assertion(PIZZA_TYPE.PEPPERONI);
   });
 
   it(`[${seq++}] 시카고 치즈 피자 생성`, function () {
-    pizzaFactoryAssertion(chicagoStore, PIZZA_STYLE.CHICAGO, PIZZA_TYPE.CHEESE);
-    pizzaFactoryAssertion(chicagoStore, PIZZA_STYLE.CHICAGO, PIZZA_TYPE.CLAM);
-    pizzaFactoryAssertion(chicagoStore, PIZZA_STYLE.CHICAGO, PIZZA_TYPE.VEGGIE);
-    pizzaFactoryAssertion(
-      chicagoStore,
-      PIZZA_STYLE.CHICAGO,
-      PIZZA_TYPE.PEPPERONI
-    );
     const ingredientFactory: PizzaIngredientFactory =
       new ChicagoPizzaIngredientFactory();
 
+    const assertion = buildAssertion(ingredientFactory, PIZZA_STYLE.CHICAGO);
+    assertion(PIZZA_TYPE.CHEESE);
+    assertion(PIZZA_TYPE.CLAM);
+    assertion(PIZZA_TYPE.VEGGIE);
+    assertion(PIZZA_TYPE.PEPPERONI);
   });
 });
