44f1e022ff85e0335ad232ce13f92b4d02d0858b
mercuriver <mercuriver@gmail.com>
2022-12-18 17:50:16 +0900

    feat(command): TV 커멘드 구상 클래스 export 처리

--- patterns/command/ConcreteCommand/index.ts
+++ patterns/command/ConcreteCommand/index.ts
@@ -1,4 +1,8 @@
 export { default as LightOnCommand } from "./LightOnCommand";
 export { default as LightOffCommand } from "./LightOffCommand";
+
+export { default as TVOnCommand } from "./TVOnCommand";
+export { default as TVOffCommand } from "./TVOffCommand";
+
 export { default as NoCommand } from "../ConcreteCommand/NoCommand";
 export { default as MacroCommand } from "../ConcreteCommand/MacroCommand";
