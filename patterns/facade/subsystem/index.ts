f3aebb6572bdedacb53acaed4116148a95bb0bc5
mercuriver <mercuriver@gmail.com>
2022-12-22 16:10:13 +0900

    feat(facade): 서브시스템 export 정리

---
+++ patterns/facade/subsystem/index.ts
@@ -0,0 +1,8 @@
+export { default as Amplifier } from "./Amplifier";
+export { default as Tuner } from "./Tuner";
+export { default as StreamingPlayer } from "./StreamingPlayer";
+export { default as CdPlayer } from "./CdPlayer";
+export { default as Projector } from "./Projector";
+export { default as TheaterLights } from "./TheaterLights";
+export { default as Screen } from "./Screen";
+export { default as PopcornPopper } from "./PopcornPopper";
