44b9f350fd39a9889cefd3c251a1b68d05e53dc9
mercuriver <mercuriver@gmail.com>
2022-12-22 11:00:46 +0900

    feat(facade): 서브시스템 클래스 작성 #1

---
+++ patterns/facade/subsystem/StreamingPlayer.ts
@@ -0,0 +1,75 @@
+import Amplifier from "./Amplifier";
+
+class StreamingPlayer {
+  #description: string;
+  #currentChapter: number;
+  #amplifier: Amplifier;
+  #movie: string;
+
+  constructor(description: string, amplifier: Amplifier) {
+    this.#description = description;
+    this.#amplifier = amplifier;
+  }
+
+  on(): void {
+    console.log(this.#description + " on");
+  }
+
+  off(): void {
+    console.log(this.#description + " off");
+  }
+
+  // method overloading
+  // https://stackoverflow.com/questions/31810881/typescript-overloading-class-methods-same-return-type-different-parameters
+  play(movie: string): void;
+  play(chapter: number): void;
+  play(movieOrChapter: string | number): void {
+    if (typeof movieOrChapter === "string") {
+      this.#movie = movieOrChapter;
+      this.#currentChapter = 0;
+      console.log(this.#description + ' playing "' + movieOrChapter + '"');
+    } else {
+      if (this.#movie == null) {
+        console.log(
+          this.#description +
+            " can't play chapter " +
+            movieOrChapter +
+            " no movie selected"
+        );
+      } else {
+        this.#currentChapter = movieOrChapter;
+        console.log(
+          this.#description +
+            " playing chapter " +
+            this.#currentChapter +
+            ' of "' +
+            this.#movie +
+            '"'
+        );
+      }
+    }
+  }
+
+  stop(): void {
+    this.#currentChapter = 0;
+    console.log(this.#description + ' stopped "' + this.#movie + '"');
+  }
+
+  pause(): void {
+    console.log(this.#description + ' paused "' + this.#movie + '"');
+  }
+
+  setTwoChannelAudio(): void {
+    console.log(this.#description + " set two channel audio");
+  }
+
+  setSurroundAudio(): void {
+    console.log(this.#description + " set surround audio");
+  }
+
+  toString(): string {
+    return this.#description;
+  }
+}
+
+export default StreamingPlayer;
