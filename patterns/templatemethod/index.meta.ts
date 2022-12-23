ada917f2c3b3479d9a86b10739fe07adf7ab94e3
mercuriver <mercuriver@gmail.com>
2022-12-23 18:02:51 +0900

    feat(templateMethod): 음료 구성요소 meta 데이터 작성

---
+++ patterns/templatemethod/index.meta.ts
@@ -0,0 +1,16 @@
+const common = {
+  boilWater: "Boiling water",
+  pourInCup: "Pouring into cup",
+};
+
+const coffee = {
+  brew: "Dripping Coffee through filter",
+  condiments: "Adding Sugar and Milk",
+};
+
+const tea = {
+  brew: "Steeping the tea",
+  condiments: "Adding Lemon",
+};
+
+export { common, coffee, tea };
