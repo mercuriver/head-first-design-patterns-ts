ef2df7906072b02493d35efb33f8f045293bf4eb
mercuriver <mercuriver@gmail.com>
2022-12-24 20:26:54 +0900

    feat(iterator): MenuItme 클래스 구현

---
+++ patterns/iterator/MenuItem.ts
@@ -0,0 +1,40 @@
+class MenuItem {
+  #name: string;
+  #description: string;
+  #vegetarian: boolean;
+  #price: number;
+
+  constuctor(
+    name: string,
+    description: string,
+    vegetarian: boolean,
+    price: number
+  ) {
+    this.#name = name;
+    this.#description = description;
+    this.#vegetarian = vegetarian;
+    this.#price = price;
+  }
+
+  get name(): string {
+    return this.#name;
+  }
+
+  get description(): string {
+    return this.#description;
+  }
+
+  get price(): number {
+    return this.#price;
+  }
+
+  isVegetarian(): boolean {
+    return this.#vegetarian;
+  }
+
+  toString(): string {
+    return this.#name + ", $" + this.#price + "\n   " + this.#description;
+  }
+}
+
+export default MenuItem;
