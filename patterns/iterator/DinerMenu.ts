20c3a2edb402657ec71ea01b5ce27f386d9413bd
mercuriver <mercuriver@gmail.com>
2022-12-25 14:28:17 +0900

    feat(iterator): DinerMenu 구현 #comp

--- patterns/iterator/DinerMenu.ts
+++ patterns/iterator/DinerMenu.ts
@@ -13,10 +13,62 @@ class DinerMenu implements Menu<MenuItem> {
     this.#menuItems = new MenuItem[MAX_ITEMS]();
 
     this.#numberOfItems = 0;
+
+    addItem(
+      "Vegetarian BLT",
+      "(Fakin') Bacon with lettuce & tomato on whole wheat",
+      true,
+      2.99
+    );
+    addItem("BLT", "Bacon with lettuce & tomato on whole wheat", false, 2.99);
+    addItem(
+      "Soup of the day",
+      "Soup of the day, with a side of potato salad",
+      false,
+      3.29
+    );
+    addItem(
+      "Hotdog",
+      "A hot dog, with sauerkraut, relish, onions, topped with cheese",
+      false,
+      3.05
+    );
+    addItem(
+      "Steamed Veggies and Brown Rice",
+      "A medly of steamed vegetables over brown rice",
+      true,
+      3.99
+    );
+    addItem(
+      "Pasta",
+      "Spaghetti with Marinara Sauce, and a slice of sourdough bread",
+      true,
+      3.89
+    );
+  }
+
+  addItem(
+    name: string,
+    description: string,
+    vegetarian: boolean,
+    price: number
+  ): void {
+    const menuItem = new MenuItem(name, description, vegetarian, price);
+
+    if (this.#numberOfItems >= MAX_ITEMS) {
+      console.log("Sorry, menu is full!  Can't add item to menu");
+    } else {
+      this.#menuItems[this.#numberOfItems] = menuItem;
+      this.#numberOfItems = this.#numberOfItems + 1;
+    }
   }
 
   createIterator(): Iterator<MenuItem> {
     return null;
+  }
+
+  getMenuItems(): MenuItem[] {
+    return this.#menuItems;
   }
 }
 
