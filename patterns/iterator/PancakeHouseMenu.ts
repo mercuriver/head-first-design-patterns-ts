bb5f29b9f62f15f2543e63b304cecd68854d6b6b
mercuriver <mercuriver@gmail.com>
2022-12-25 15:05:54 +0900

    feat(iterator): PancakeHouseMenu 구현

---
+++ patterns/iterator/PancakeHouseMenu.ts
@@ -0,0 +1,59 @@
+import CustomIterator from "./CustomIterator";
+import Menu from "./Menu";
+import MenuItem from "./MenuItem";
+
+class PancakeHouseMenu implements Menu<MenuItem> {
+  #menuItems: Set<MenuItem>;
+
+  constructor() {
+    this.#menuItems = new Set<MenuItem>();
+
+    this.addItem(
+      "K&B's Pancake Breakfast",
+      "Pancakes with scrambled eggs, and toast",
+      true,
+      2.99
+    );
+
+    this.addItem(
+      "Regular Pancake Breakfast",
+      "Pancakes with fried eggs, sausage",
+      false,
+      2.99
+    );
+
+    this.addItem(
+      "Blueberry Pancakes",
+      "Pancakes made with fresh blueberries, and blueberry syrup",
+      true,
+      3.49
+    );
+
+    this.addItem(
+      "Waffles",
+      "Waffles, with your choice of blueberries or strawberries",
+      true,
+      3.59
+    );
+  }
+
+  addItem(
+    name: string,
+    description: string,
+    vegetarian: boolean,
+    price: number
+  ): void {
+    const menuItem = new MenuItem(name, description, vegetarian, price);
+    this.#menuItems.add(menuItem);
+  }
+
+  get menuItems(): Set<MenuItem> {
+    return this.#menuItems;
+  }
+
+  createIterator(): CustomIterator<MenuItem> {
+    return null;
+  }
+}
+
+export default PancakeHouseMenu;
