e2adaa23415fd566b8a396a9973c4630229a7837
mercuriver <mercuriver@gmail.com>
2022-12-26 17:02:23 +0900

    feat(iterator): 테스트를 위한 메뉴명 메타화

---
+++ patterns/iterator/index.meta.ts
@@ -0,0 +1,17 @@
+const dinerMenuName = {
+  VEGETARIAN_BLT: "Vegetarian BLT",
+  BLT: "BLT",
+  SOUP_OF_THE_DAY: "Soup of the day",
+  HOTDOG: "Hotdog",
+  STEAMED_VEGGIES_AND_BROWN_RICE: "Steamed Veggies and Brown Rice",
+  PASTA: "Pasta",
+};
+
+const pancakeHouseMenuName = {
+  KBS_PANCAKE_BREAKFAST: "K&B's Pancake Breakfast",
+  REGULAR_PANCAKE_BREAKFAST: "Regular Pancake Breakfast",
+  BLUEBERRY_PANCAKES: "Blueberry Pancakes",
+  WAFFLES: "Waffles",
+};
+
+export { dinerMenuName, pancakeHouseMenuName };
