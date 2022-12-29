7269930f934b801e0a8d9215930355eaf9b3c74a
mercuriver <mercuriver@gmail.com>
2022-12-29 14:03:13 +0900

    feat(composite): MenuItem 구상 클래스 작성

---
+++ patterns/composite/Leaf/MenuItem.ts
@@ -0,0 +1,45 @@
+import MenuComponent from "../Component/MenuComponent";
+
+class MenuItem extends MenuComponent {
+  #name: string;
+  #description: string;
+  #vegetarian: boolean;
+  #price: number;
+
+  constructor(
+    name: string,
+    description: string,
+    vegetarian: boolean,
+    price: number
+  ) {
+    super();
+    this.#name = name;
+    this.#description = description;
+    this.#vegetarian = vegetarian;
+    this.#price = price;
+  }
+
+  get name(): string {
+    return this.#name;
+  }
+
+  get description(): string {
+    return this.#description;
+  }
+
+  get price(): number {
+    return this.#price;
+  }
+
+  isVegetarian(): boolean {
+    return this.#vegetarian;
+  }
+
+  print(): string {
+    return `  ${this.name}${this.isVegetarian() ? "(v)" : ""}, ${
+      this.price
+    }     -- ${this.description}`;
+  }
+}
+
+export default MenuItem;
